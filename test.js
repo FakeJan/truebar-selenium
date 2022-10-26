9e157f5b9a5428251ba073e8f887dc7a6dc733ce
FakeJan <jan.ivanetic10@gmail.com>
2022-10-26 17:26:30 +0200

    Fixed version

--- test.js
+++ test.js
@@ -1,100 +1,99 @@
-(async function Truebar() {
-  // Knjižnice
-  const { execSync } = require("child_process");
-  const { describe, it, after, before } = require("mocha");
-  const { Builder, By } = require("selenium-webdriver");
-  const chrome = require("selenium-webdriver/chrome");
-  const expect = require("chai").expect;
-  const driver = require('selenium-webdriver')
+const chrome = require("selenium-webdriver/chrome");
+const { Builder, By, Key, until } = require("selenium-webdriver");
 
-  let aplikacijaUrl = "https://staging-editor.true-bar.si/";
-  //let seleniumStreznikUrl = "http://localhost:4444/wd/hub";
-  let seleniumStreznikUrl = "http://localhost:4444/";
-  let brskalnik;
+(async function googleSearch() {
+  let driver = await new Builder()
+    .forBrowser("chrome")
+    // .usingServer("http://localhost:4444/wd/hub/")
+    .setChromeOptions(
+      new chrome.Options()
+        .addArguments("incognito")
+        .addArguments("--lang=en")
+        .addArguments("start-maximized")
+        .addArguments("disable-infobars")
+        // .addArguments("--headless")
+        .addArguments("--disable-dev-shm-usage")
+        .addArguments("--no-sandbox")
+        .addArguments("allow-file-access-from-files")
+        .addArguments("use-fake-device-for-media-stream")
+        .addArguments("use-fake-ui-for-media-stream")
+        .addArguments("--allow-file-access")
+        .addArguments("--use-file-for-fake-audio-capture")
+    )
+    .setChromeService(
+      chrome.setDefaultService(
+        new chrome.ServiceBuilder("./drivers/chromedriver.exe").build()
+      )
+    )
+    .build();
 
-  // Obvladovanje napak
-  process.on("unhandledRejection", (napaka) => {
-    console.log(napaka);
-  });
+
+
+  // on('before:browser:launch', (browser = {}, launchOptions = { args: [] }) => {
+  //   if (browser.name === 'chrome') {
+  //     // https://bugs.chromium.org/p/chromium/issues/detail?id=1032604#c37
+  //     launchOptions.args.push('--no-sandbox')
+  //     launchOptions.args.push('--allow-file-access-from-files')
+  //     launchOptions.args.push('--use-fake-ui-for-media-stream')
+  //     launchOptions.args.push('--use-fake-device-for-media-stream')
+  //     // mp3 not work
+  //     launchOptions.args.push('--use-file-for-fake-audio-capture=tests/fixtures/hello.wav')
+  //   }
+  //   return launchOptions
+  // })
+
+  //https://peter.sh/experiments/chromium-command-line-switches/#use-file-for-fake-audio-capture
+  //https://www.google.com/search?q=how+to+use+--use-file-for-fake-audio-capture&rlz=1C1GCEJ_enSI1015SI1015&oq=how+to+use+--use-file-for-fake-audio-capture&aqs=chrome..69i57.2037j0j7&sourceid=chrome&ie=UTF-8
 
   const delay = ms => new Promise(res => setTimeout(res, ms));
-
-  // Poèakaj doloèeno število sekund na zahtevani element na strani
-  let pocakajStranNalozena = async (brskalnik, casVS, xpath) => {
-    await brskalnik.wait(
-      () => {
-        return brskalnik.findElements(By.xpath(xpath)).then((elementi) => {
-          return elementi[0];
-        });
-      },
-      casVS * 1000,
-      `Stran se ni naložila v ${casVS} s.`
-    );
-  };
-
   try {
-    before(() => {
-      brskalnik = new Builder()
-        .forBrowser("chrome")
-        .setChromeOptions(
-          new chrome.Options()
-            .addArguments("start-maximized")
-            .addArguments("disable-infobars")
-            .addArguments("allow-insecure-localhost")
-            .addArguments("allow-running-insecure-content")
-            .addArguments("--window-size=1920,1080")
-            .addArguments("--disable-dev-shm-usage")
-            .addArguments("--no-sandbox")
-          // .addArguments("--headless")
-        )
-        // .setChromeService(
-        //   chrome.setDefaultService(
-        //     new chrome.ServiceBuilder("./drivers/chromedriver.exe").build()
-        //   )
-        // )
-        .usingServer(seleniumStreznikUrl)
-        .build();
-    });
+    let aplikacijaUrl = "https://staging-editor.true-bar.si/";
+    console.log("x");
 
-    describe("Funcionalni test za prijavo", function () {
-      this.timeout(30 * 1000);
-      before(() => {
-        brskalnik.get(aplikacijaUrl);
-      });
+    // Navigate to Url
+    await driver.get(aplikacijaUrl);
+    await delay(1000)
+    //login
+    await driver
+      .findElement(By.xpath("/html/body/div/div/main/div/form/div[1]/div[2]/div[2]/div[1]/div/input"))
+      .sendKeys("jani");
 
-      it("Začetna stran aplikacije naložena", async () => {
-        await pocakajStranNalozena(brskalnik, 20, "//h2");
-        let prijavaGumb = await brskalnik.findElement(By.xpath("/html/body/div/div/main/div/form/div[2]/div/button"));
-        expect(prijavaGumb).should.exist;
-      });
+    await delay(1000)
+    await driver
+      .findElement(By.xpath("/html/body/div/div/main/div/form/div[1]/div[2]/div[2]/div[2]/div/input"))
+      .sendKeys("mIqrfAx490@6");
+    // // login button
+    await delay(1000)
+    await driver
+      .findElement(By.xpath("/html/body/div[1]/div/main/div/form/div[2]/div/button"))
+      .click();
 
-      it("Prijava", async () => {
+    await delay(1000)
+    await driver
+      .findElement(By.xpath("/html/body/div/div/main/div/form/button"))
+      .click();
+    // v zivo button
+    await delay(1000)
+    await driver
+      .findElement(By.xpath("/html/body/div/div/main/div/div[1]/div/div/button[1]"))
+      .click();
 
-        let usernameInput = await brskalnik.findElement(By.xpath("/html/body/div/div/main/div/form/div[1]/div[2]/div[2]/div[1]/div/input"));
-        await usernameInput.sendKeys("jani");
+    let record = await driver
+      .findElement(By.xpath("/html/body/div/div/main/div/div[3]/div/div/div/div/div[2]/div[2]/button"))
+      .getAttribute();
 
-        let passInput = await brskalnik.findElement(By.xpath("/html/body/div/div/main/div/form/div[1]/div[2]/div[2]/div[2]/div/input"));
-        passInput.sendKeys("mIqrfAx490@6");
+    console.log(record);
 
-        let loginButton = await brskalnik.findElement(By.xpath("/html/body/div/div/main/div/form/div[2]/div/button"));
-        await loginButton.click();
+    //need to play audio
+    //https://stackoverflow.com/questions/9419263/how-to-play-audio
+    // var audio = new Audio('trplenje.wav');
+    // audio.play();
 
-        let xButton = await brskalnik.findElement(By.xpath("/html/body/div/div/main/div/form/button/svg"));
-        await xButton.click();
 
-        // let vZivo = await brskalnik.findElement(By.xpath("/html/body/div/div/main/div/div[1]/div/div/button[1]"));
-        // await vZivo.click();
-
-        // play audio file
-
-      });
-
-    });
-    after(async () => {
-      brskalnik.quit();
-    });
-  } catch (napaka) {
-    console.log(napaka);
-    //console.log("Med testom je prišlo do napake!");
+    //console.log(await firstResult.getAttribute("textContent"));
+    console.log(await (await driver.getCapabilities()).getBrowserName());
+    console.log(await (await driver.getCapabilities()).getBrowserVersion());
+  } finally {
+    //driver.quit();
   }
 })();
